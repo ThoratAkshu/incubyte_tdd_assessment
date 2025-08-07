2a06e99d4da40c7918605aecb0ee97ee3fd6ba30
ThoratAkshu <86485909+ThoratAkshu@users.noreply.github.com>
2025-08-07 11:41:01 +0530

    add test for one number

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,7 +1,12 @@
-#  add test for empty string
+
 import unittest
 from string_calculator import add
 
 class TestStringCalculator(unittest.TestCase):
+    #  add test for empty string
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
+
+    # add test for one number
+    def test_one_number(self):
+        self.assertEqual(add("1"), 1)
